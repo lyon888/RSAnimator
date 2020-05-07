96e90bb7338b8d91b594cce325f7edae107a9b89
lyonLiu <lyonLiu_job@163.com>
2020-05-07 15:57:57 +0800

    fix bug

--- RSAnimator/Classes/YSAnimator.m
+++ RSAnimator/Classes/YSAnimator.m
@@ -39,9 +39,9 @@
     if (self.type == YSAnimatorPopTypeScale){
         self.objView.contentView.transform = CGAffineTransformMakeScale(0.00001, 0.00001);
     }else if (self.type == YSAnimatorPopTypeUpToBottom){
-        self.objView.contentView.transform = CGAffineTransformMakeTranslation(0, -self.objView.contentView.height);
+        self.objView.contentView.transform = CGAffineTransformMakeTranslation(0, -self.objView.contentView.frame.size.height);
     }else if (self.type == YSAnimatorPopTypeBottomToUp){
-        self.objView.contentView.transform = CGAffineTransformMakeTranslation(0, self.objView.contentView.height);
+        self.objView.contentView.transform = CGAffineTransformMakeTranslation(0, self.objView.contentView.frame.size.height);
     }
     [UIView animateWithDuration:.25 animations:^{
         self.objView.alpha = 1;
@@ -59,9 +59,9 @@
         if (self.type == YSAnimatorPopTypeScale){
             self.objView.contentView.transform = CGAffineTransformMakeScale(0.00001, 0.00001);
         }else if (self.type == YSAnimatorPopTypeUpToBottom){
-            self.objView.contentView.transform = CGAffineTransformMakeTranslation(0, -self.objView.contentView.height);
+            self.objView.contentView.transform = CGAffineTransformMakeTranslation(0, -self.objView.contentView.frame.size.height);
         }else if (self.type == YSAnimatorPopTypeBottomToUp){
-            self.objView.contentView.transform = CGAffineTransformMakeTranslation(0, self.objView.contentView.height);
+            self.objView.contentView.transform = CGAffineTransformMakeTranslation(0, self.objView.contentView.frame.size.height);
         }
     } completion:^(BOOL finished) {
         [self.objView removeFromSuperview];
